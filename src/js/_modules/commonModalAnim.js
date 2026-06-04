2c9aa75937328e55345d3024fe7d6e8e5f6aedd7
mizuyachihiro <mizuya@maam.me>
2026-06-04 11:44:06 +0900

    補足説明追加とレイアウト調整

--- src/js/_modules/commonModalAnim.js
+++ src/js/_modules/commonModalAnim.js
@@ -1,19 +1,19 @@
 export default class CommonModalAnim {
   openModal(modal){
     if(!modal) return;  
-    modal.classList.add('is-open')
-    modal.classList.add('is-loading')
+    modal.classList.add('is-open') // bgが表示トリガークラス
+    modal.classList.add('is-loading') // loadingアニメーショントリガークラス
     const c = modal.querySelector('.l-modal__wrapper')
 
     document.body.style.overflow = 'hidden';
     modal.scrollTop = 0;
     if(c) c.scrollTop = 0;
     setTimeout(()=>{
-      modal.classList.add('is-loaded')
+      modal.classList.add('is-loaded')// モーダル中身の表示トリガークラス
       modal.scrollTop = 0;
       if(c) c.scrollTop = 0;
       setTimeout(()=> {
-        modal.classList.remove('is-loading')
+        modal.classList.remove('is-loading')// 中身が表示された後にローディングアニメーションを消す
       }, 300)
     }, 1000)
   }
