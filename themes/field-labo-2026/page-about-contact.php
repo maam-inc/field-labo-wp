df9d76f933bf63404286fcadd20c4032d191e722
Hitomi Chinatsu <hitomi@maam.me>
2026-08-27 13:58:21 +0900

    about WP化

--- themes/field-labo-2026/page-about-contact.php
+++ themes/field-labo-2026/page-about-contact.php
@@ -11,23 +11,38 @@
         <div class="about__wrapper">
           <div class="about__block about__block--1">
             <h2 class="about__head f-inter-B">message</h2>
-            <p class="about__text f-noto-M">新たな住まいを探し始めたけど、<br>どれも何か違う気がする<br>何が違うのかはよくわからないし、<br>はっきりとしたイメージがあるわけでもない・・<br>そんな想いを形にするお手伝いをします。<br><br>設計デザイン、施工監理、現場監督まで隅々まで自身で監理できる事で満足できるコストパフォーマンスを発揮できます。<br>案件ごとに異なる条件・ご要望・予算を加味しながらバランスのとれたポイントを探し出し、提供することをお約束します。</p>
+            <?php $message = get_field('message'); ?>
+            <p class="about__text f-noto-M"><?php echo nl2br($message); ?></p>
+            <!-- <p class="about__text f-noto-M">新たな住まいを探し始めたけど、<br>どれも何か違う気がする<br>何が違うのかはよくわからないし、<br>はっきりとしたイメージがあるわけでもない・・<br>そんな想いを形にするお手伝いをします。<br><br>設計デザイン、施工監理、現場監督まで隅々まで自身で監理できる事で満足できるコストパフォーマンスを発揮できます。<br>案件ごとに異なる条件・ご要望・予算を加味しながらバランスのとれたポイントを探し出し、提供することをお約束します。</p> -->
           </div>
           <div class="about__block about__block--2">
             <h2 class="about__head f-inter-B">about me</h2>
             <div class="about__text-group f-noto-M">
-              <p class="about__name f-noto-B">柴田 茂穂<span class="en f-inter-R">shigeho.shibata</span></p>
-              <p class="about__text">1971年 東京都生まれ<br><br>学校法人 環境造形学園 ICSカレッジオブアーツ（旧インテリアセンタースクール）を卒業後、<br>1995年 株式会社ヨシノスペースデベロップメントにて大手マンションデベロッパーをクライアントに営業・ショールームの・企画デザイン・工事監督を務める。<br><br>2007年 ゆくい堂株式会社 に転籍、設計デザイナー案件の工事担当・現場監理としてリノベーション案件に関わり、その後同社にて主に個人クライアントの住宅・事業用の賃貸及びリニューアル販売案件のリノベーション設計デザイン・現場監理・工事監督 等の業務を務める。<br><br>2016年 FIELD LABO として活動を開始。<br>[主な業務内容] 住宅（マンション・戸建）店舗・事務所のリノベーションの設計デザイン及び施工監理。<br>オリジナル家具、プロダクトの制作販売。趣味は仕事とスイッチなど小物アイテムの創作。</p>
+              <?php $about_me = get_field('about_me'); ?>
+              <p class="about__name f-noto-B"><?php echo esc_html($about_me['name']); ?><span class="en f-inter-R"><?php echo esc_html($about_me['name_en']); ?></span></p>
+              <p class="about__text"><?php echo nl2br(esc_html($about_me['detail'])); ?></p>
+              <!-- <p class="about__name f-noto-B">柴田 茂穂<span class="en f-inter-R">shigeho.shibata</span></p>
+              <p class="about__text">1971年 東京都生まれ<br><br>学校法人 環境造形学園 ICSカレッジオブアーツ（旧インテリアセンタースクール）を卒業後、<br>1995年 株式会社ヨシノスペースデベロップメントにて大手マンションデベロッパーをクライアントに営業・ショールームの・企画デザイン・工事監督を務める。<br><br>2007年 ゆくい堂株式会社 に転籍、設計デザイナー案件の工事担当・現場監理としてリノベーション案件に関わり、その後同社にて主に個人クライアントの住宅・事業用の賃貸及びリニューアル販売案件のリノベーション設計デザイン・現場監理・工事監督 等の業務を務める。<br><br>2016年 FIELD LABO として活動を開始。<br>[主な業務内容] 住宅（マンション・戸建）店舗・事務所のリノベーションの設計デザイン及び施工監理。<br>オリジナル家具、プロダクトの制作販売。趣味は仕事とスイッチなど小物アイテムの創作。</p> -->
             </div>
           </div>
           <div class="about__block about__block--3">
             <h2 class="about__head f-inter-B">faq</h2>
-            <p class="about__text f-noto-M">FIELD LABO のリノベーションについて<br class="sp">Q&amp;A形式にまとめました。<br><a href="/faq" class="about__text-link">こちら</a>をご覧ください。</p>
+            <?php $faq = get_field('faq'); ?>
+            <p class="about__text f-noto-M"><?php echo nl2br($faq); ?></p>
+            <!-- <p class="about__text f-noto-M">FIELD LABO のリノベーションについて<br class="sp">Q&amp;A形式にまとめました。<br><a href="/faq" class="about__text-link">こちら</a>をご覧ください。</p> -->
           </div>
           <div class="about__block about__block--4">
             <h2 class="about__head f-inter-B">contact</h2>
+            <?php $contact = get_field('contact'); ?>
             <div class="about__contacts">
-              <div class="about__contact">
+              <?php $contact = get_field('contact'); ?>
+              <?php foreach($contact['contact_list'] as $item ) : ?>
+              <dl class="about__contact">
+                <dd class="head f-inter-B"><?php echo esc_html($item['label']); ?></dd>
+                <dt class="text f-noto-M"><?php echo esc_html($item['detail']); ?></dt>
+              </dl>
+              <?php endforeach; ?>
+              <!-- <div class="about__contact">
                 <p class="head f-inter-B">address</p>
                 <p class="text f-noto-M">〒205-0014 東京都羽村市羽東2-2-3</p>
               </div>
@@ -46,10 +61,10 @@
               <div class="about__contact">
                 <p class="head f-inter-B">website</p>
                 <p class="text f-noto-M">http://www.field-labo.com/</p>
-              </div>
+              </div> -->
             </div>
-            <p class="about__note f-noto-M">
-              ※主に東京都及び首都圏近郊にて活動しております。<br class="sp">お気軽にお問い合わせください。</p>
+            <p class="about__note f-noto-M"><?php echo nl2br($contact['contact_note']); ?></p>
+            <!-- <p class="about__note f-noto-M">※主に東京都及び首都圏近郊にて活動しております。<br class="sp">お気軽にお問い合わせください。</p> -->
           </div>
         </div>
       </div>
